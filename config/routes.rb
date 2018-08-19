a51ffaf3331e9550c5a10acb684c092ffccd2388
faxathome <parafajardo@gmail.com>
2018-08-19 23:57:14 +0200

    Add Users & Stocks many to many and add Stocks to users via UserStocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # root 'welcome#index'
-  root 'users#my_portfolio'
+  root 'welcome#index'
+  # root 'users#my_portfolio'
 
 get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stocks', to: 'stocks#search'
+resources :user_stocks, only: [:create]
 
 end
